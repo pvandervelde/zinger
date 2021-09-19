5b321835564c188f2bff3b1bf841c815d412c599
Petrik van der Velde <petrikvandervelde@gmail.com>
2021-09-19 22:59:08 +1200

    Graphs for acceleration

--- robot_functions.py
+++ robot_functions.py
@@ -1,4 +1,5 @@
 import math
+import numpy
 
 gravitational_acceleration = 9.80665
 air_density = 1.225
@@ -12,9 +13,23 @@ def force_rolling_friction(mass_in_kg, coefficient_of_friction, slope_angle):
 def force_gravity_normal_to_surface(mass_in_kg, slope_angle):
     return mass_in_kg * gravitational_acceleration * math.cos(math.radians(slope_angle))
 
-def force_gravity_parallel_to_surface(mass_in_kg, hill_angle):
-    return mass_in_kg * gravitational_acceleration * math.sin(math.radians(hill_angle))
+def force_gravity_parallel_to_surface(mass_in_kg, slope_angle):
+    return mass_in_kg * gravitational_acceleration * math.sin(math.radians(slope_angle))
 
+def velocity_for_slope_and_power(power, area_in_msq, drag_coefficient, mass_in_kg, coefficient_of_friction, slope_angle):
+    # F_aero * V + F_roll * V + F_hill * V - P = 0
+    coeff_cube = 0.5 * air_density * area_in_msq * drag_coefficient
+    coeff_square = 0.0
+    coeff_lin = force_rolling_friction(mass_in_kg, coefficient_of_friction, slope_angle) + force_gravity_parallel_to_surface(mass_in_kg, slope_angle)
+
+    roots = numpy.roots([coeff_cube, coeff_square, coeff_lin, -power])
+
+    real_value = 0.0
+    for i in range(len(roots)):
+     if numpy.isreal(roots[i]):
+         real_value = numpy.real(roots[i])
+
+    return real_value
 
 def wheel_load(
     area_in_msq,
@@ -42,6 +57,11 @@ def wheel_load(
     power = wheel_force * velocity_in_meters_per_second
 
     return wheel_force, power
+
+
+
+
+
 
 def electrical_efficiency(battery_to_controller_efficiency,
                           controller_to_motor_efficiency, motor_efficiency,
