af960cd6a8d161fa8e130c9bb7da7e6b914fece6
Petrik van der Velde <petrikvandervelde@gmail.com>
2021-10-25 22:17:16 +1300

    refactoring the motor calculation files to use the neo motor curves

---
+++ crate_rover_specs.py
@@ -0,0 +1,128 @@
+# imports
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+class Coordinate:
+
+    def __init__(self, x: float, y: float, z: float):
+        self.x = x
+        self.y = y
+        self.z = z
+
+class BoundingBox:
+
+    def __init__(self, length_in_meters: float, width_in_meters: float, height_in_meters: float):
+        self.length_in_meters = length_in_meters
+        self.width_in_meters = width_in_meters
+        self.height_in_meters = height_in_meters
+
+    def center_of_gravity(self) -> Coordinate:
+        return Coordinate(self.length_in_meters / 2.0, self.width_in_meters / 2.0, self.height_in_meters / 2.0)
+
+class Cargo:
+    ''' Stores information about an item of cargo transported by the CrateRover. '''
+
+    def __init__(self, bounding_box: BoundingBox, maximum_weight_in_kg: float):
+        self.bounding_box = bounding_box
+        self.maximum_weight_in_kg = maximum_weight_in_kg
+
+    def center_of_gravity(self) -> Coordinate:
+        return self.bounding_box.center_of_gravity()
+
+    def to_dataframe(self) -> pd.DataFrame:
+        ''' Prints a table of information describing the Cargo. '''
+        crate_data = [["Maximum weight (kg)", self.maximum_weight_in_kg],
+              ["Length (m)", self.bounding_box.length_in_meters],
+              ["Width (m)", self.bounding_box.width_in_meters],
+              ["Height (m)", self.bounding_box.height_in_meters]]
+        return pd.DataFrame(crate_data, columns=["Cargo", "Value"])
+
+class RoverWheel:
+
+    def __init__(self, radius_in_meters: float):
+        self.radius_in_meters = radius_in_meters
+
+class RoverChassis:
+
+    def __init__(self, bounding_box: BoundingBox, weight_in_kg: float, number_of_wheels: int, wheel: RoverWheel):
+        self.bounding_box = bounding_box
+        self.weight_in_kg = weight_in_kg
+        self.number_of_wheels = number_of_wheels
+        self.wheel = wheel
+
+    def center_of_gravity(self) -> Coordinate:
+        return self.bounding_box.center_of_gravity()
+
+class RoverPerformance:
+
+    def __init__(
+        self,
+        min_velocity_on_flat_ground_in_meterspersecond: float,
+        min_velocity_on_sloped_ground_in_meterspersecond: float,
+        maximum_slope_angle_with_maximum_load: float,
+        maximum_slope_angle_without_load: float,
+        minimum_battery_life_in_hours: float
+    ):
+        self.min_velocity_on_flat_ground = min_velocity_on_flat_ground_in_meterspersecond
+        self.min_velocity_on_sloped_ground = min_velocity_on_sloped_ground_in_meterspersecond
+        self.max_slope_angle_with_maximum_load = maximum_slope_angle_with_maximum_load
+        self.max_slope_angle_without_load = maximum_slope_angle_without_load
+        self.minimum_battery_life_in_hours = minimum_battery_life_in_hours
+
+    def maximum_slope_angle_with_maximum_load(self) -> float:
+        return self.max_slope_angle_with_maximum_load
+
+    def maximum_slope_angle_without_load(self) -> float:
+        return self.max_slope_angle_without_load
+
+    def maximum_velocity_on_flat_ground(self) -> float:
+        return self.min_velocity_on_flat_ground
+
+    def maximum_velocity_on_sloped_ground(self) -> float:
+        return self.min_velocity_on_sloped_ground
+
+    def to_dataframe(self) -> pd.DataFrame:
+        performance_data = [
+            ["Minimum speed on the flat with load (m/s)", self.min_velocity_on_flat_ground],
+            ["Minimum speed on a slope with load (m/s)", self.min_velocity_on_flat_ground],
+            ["Maximum hill angle without load (degrees)", self.max_slope_angle_without_load],
+            ["Maximum hill angle with load (degrees)", self.max_slope_angle_with_maximum_load],
+            ["Minimum battery life (hours)", self.minimum_battery_life_in_hours]
+        ]
+        return pd.DataFrame(performance_data, columns=["Performance", "Value"])
+
+class CrateRoverSpecifications:
+
+    def __init__(
+        self,
+        cargo: Cargo,
+        expectedPerformance: RoverPerformance,
+        chassis: RoverChassis):
+        self.cargo = cargo
+        self.performance = expectedPerformance
+        self.chassis = chassis
+
+def RoverSpecification() -> CrateRoverSpecifications:
+    cargo = Cargo(
+        BoundingBox(
+            0.520,
+            0.350,
+            0.268),
+        50.0)
+
+    performance = RoverPerformance(
+        2.0,
+        2.0,
+        15.0,
+        30.0,
+        5.0)
+
+    rover_wheel = RoverWheel(0.125)
+    rover_chassis = RoverChassis(
+        BoundingBox(0.6, cargo.bounding_box.width_in_meters + 0.05, rover_wheel.radius_in_meters * 2 + 0.10),
+        20.0,
+        4,
+        rover_wheel)
+
+    return CrateRoverSpecifications(cargo, performance, rover_chassis)
--- robot_functions.py
+++ robot_functions.py
@@ -2,6 +2,8 @@ import math
 import numpy
 import pandas as pd
 
+from crate_rover_specs import *
+
 air_density = 1.225
 
 class Physics:
@@ -14,177 +16,105 @@ class Physics:
     def gravitational_acceleration():
         return 9.80665
 
-class Coordinate:
+    def force_aerodynamic_friction(area_in_msq, velocity_in_meters_per_second):
+        return 0.5 * air_density * area_in_msq * Physics.coefficient_of_aerodynamic_drag() * math.pow(velocity_in_meters_per_second, 2)
 
-    def __init__(self, x: float, y: float, z: float):
-        self.x = x
-        self.y = y
-        self.z = z
+    def force_rolling_friction(mass_in_kg, slope_angle):
+        return Physics.coefficient_of_friction() * Physics.force_gravity_normal_to_surface(mass_in_kg, slope_angle)
 
-class BoundingBox:
+    def force_gravity_normal_to_surface(mass_in_kg, slope_angle):
+        return mass_in_kg * Physics.gravitational_acceleration() * math.cos(math.radians(slope_angle))
 
-    def __init__(self, length_in_meters: float, width_in_meters: float, height_in_meters: float):
-        self.length_in_meters = length_in_meters
-        self.width_in_meters = width_in_meters
-        self.height_in_meters = height_in_meters
+    def force_gravity_parallel_to_surface(mass_in_kg, slope_angle):
+        return mass_in_kg * Physics.gravitational_acceleration() * math.sin(math.radians(slope_angle))
 
-    def center_of_gravity(self) -> Coordinate:
-        return Coordinate(self.length_in_meters / 2.0, self.width_in_meters / 2.0, self.height_in_meters / 2.0)
-
-class Cargo:
-    ''' Stores information about an item of cargo transported by the CrateRover. '''
-
-    def __init__(self, bounding_box: BoundingBox, maximum_weight_in_kg: float):
-        self.bounding_box = bounding_box
-        self.maximum_weight_in_kg = maximum_weight_in_kg
-
-    def center_of_gravity(self) -> Coordinate:
-        return self.bounding_box.center_of_gravity()
-
-    def to_dataframe(self) -> pd.DataFrame:
-        ''' Prints a table of information describing the Cargo. '''
-        crate_data = [["Maximum weight (kg)", self.maximum_weight_in_kg],
-              ["Length (m)", self.bounding_box.length_in_meters],
-              ["Width (m)", self.bounding_box.width_in_meters],
-              ["Height (m)", self.bounding_box.height_in_meters]]
-        return pd.DataFrame(crate_data, columns=["Cargo", "Value"])
-
-class RoverPerformance:
-
-    def __init__(
-        self,
-        max_velocity_in_meterspersecond: float,
-        maximum_slope_angle_with_maximum_load: float,
-        maximum_slope_angle_without_load: float,
-        minimum_battery_life_in_hours: float
-    ):
-        self.max_velocity = max_velocity_in_meterspersecond
-        self.max_slope_angle_with_maximum_load = maximum_slope_angle_with_maximum_load
-        self.max_slope_angle_without_load = maximum_slope_angle_without_load
-        self.minimum_battery_life_in_hours = minimum_battery_life_in_hours
-
-    def maximum_slope_angle_with_maximum_load(self) -> float:
-        return self.max_slope_angle_with_maximum_load
-
-    def maximum_slope_angle_without_load(self) -> float:
-        return self.max_slope_angle_without_load
-
-    def maximum_velocity_on_flat_ground(self) -> float:
-        return self.max_velocity
-
-    def to_dataframe(self) -> pd.DataFrame:
-        performance_data = [
-            ["Maximum speed on the flat (m/s)", self.max_velocity],
-            ["Maximum hill angle without load (degrees)", self.max_slope_angle_without_load],
-            ["Maximum hill angle with load (degrees)", self.max_slope_angle_with_maximum_load],
-            ["Minimum battery life (hours)", self.minimum_battery_life_in_hours]
-        ]
-        return pd.DataFrame(performance_data, columns=["Performance", "Value"])
-
-class RoverWheel:
-
-    def __init__(self, radius_in_meters: float):
-        self.radius_in_meters = radius_in_meters
-
-class RoverChassis:
-
-    def __init__(self, bounding_box: BoundingBox, weight_in_kg: float, number_of_wheels: int, wheel: RoverWheel):
-        self.bounding_box = bounding_box
-        self.weight_in_kg = weight_in_kg
-        self.number_of_wheels = number_of_wheels
-        self.wheel = wheel
-
-    def center_of_gravity(self) -> Coordinate:
-        return self.bounding_box.center_of_gravity()
+# Coefficient of friction depending on ground material (based on material from here: https://www.roboteq.com/roboamr-2021)
+#
+# Surface type                          #       #       #
+# Concrete (good / average / bad)       0.010   0.015   0.020
+# Asphalt (good / average / bad)        0.012   0.017   0.022
+# Wood (dry / dusty / wet)              0.010   0.005   0.001
+# Snow (50mm / 100mm)                   0.025   0.037   -
+# Dirt (smooth / sandy)                 0.025   0.037   -
+# Mud (firm / medium / soft)            0.037   0.090   0.150
+# Grass (firm / soft)                   0.055   0.075   -
+# Sand (firm / soft / dune)             0.060   0.150   0.300
 
 class Rover:
     ''' Stores information about the Rover. '''
 
-    def __init__(self, chassis: RoverChassis, cargo: Cargo):
-        self.chassis = chassis
-        self.cargo = cargo
+    def __init__(self, specs: CrateRoverSpecifications):
+        self.specs = specs
+
+        self.chassis_cog = self.specs.chassis.center_of_gravity()
+        self.cargo_cog = self.specs.cargo.center_of_gravity()
+
+        self.rover_cog = Coordinate(
+            self.chassis_cog.x,
+            self.chassis_cog.y,
+            (self.chassis_cog.z * self.specs.chassis.weight_in_kg + (self.specs.chassis.bounding_box.height_in_meters + self.cargo_cog.z) * self.specs.cargo.maximum_weight_in_kg) / (self.specs.chassis.weight_in_kg + self.specs.cargo.maximum_weight_in_kg))
 
     def center_of_gravity(self) -> Coordinate:
-        chassis_cog = self.chassis.center_of_gravity()
-        cargo_cog = self.cargo.center_of_gravity()
-
-        return Coordinate(
-            chassis_cog.x,
-            chassis_cog.y,
-            (chassis_cog.z * self.chassis.weight_in_kg + (self.chassis.bounding_box.height_in_meters + cargo_cog.z) * self.cargo.maximum_weight_in_kg) / (self.chassis.weight_in_kg + self.cargo.maximum_weight_in_kg))
+        return self.rover_cog
 
     def frontal_surface(self) -> float:
-        return self.chassis.bounding_box.width_in_meters * (self.chassis.bounding_box.height_in_meters + self.cargo.bounding_box.height_in_meters)
+        return self.specs.chassis.bounding_box.width_in_meters * (self.specs.chassis.bounding_box.height_in_meters + self.specs.cargo.bounding_box.height_in_meters)
 
-    def weight_empty_in_kg(self) -> float:
-        return self.chassis.weight_in_kg
+    def weight_of_rover_chassis_in_kg(self) -> float:
+        return self.specs.specs.chassis.weight_in_kg
 
-    def weight_load_in_kg(self) -> float:
-        return self.chassis.weight_in_kg + self.cargo.maximum_weight_in_kg
+    def weight_with_cargo_in_kg(self) -> float:
+        return self.specs.chassis.weight_in_kg + self.specs.cargo.maximum_weight_in_kg
 
+    def velocity_for_slope_and_power(self, power, area_in_msq, mass_in_kg, slope_angle):
+        # F_aero * V + F_roll * V + F_hill * V - P = 0
+        coeff_cube = 0.5 * air_density * area_in_msq * Physics.coefficient_of_aerodynamic_drag()
+        coeff_square = 0.0
+        coeff_lin = Physics.force_rolling_friction(mass_in_kg, slope_angle) + Physics.force_gravity_parallel_to_surface(mass_in_kg, slope_angle)
 
-def force_aerodynamic_friction(area_in_msq, velocity_in_meters_per_second):
-    return 0.5 * air_density * area_in_msq * Physics.coefficient_of_aerodynamic_drag() * math.pow(velocity_in_meters_per_second, 2)
+        roots = numpy.roots([coeff_cube, coeff_square, coeff_lin, -power])
 
-def force_rolling_friction(mass_in_kg, slope_angle):
-    return Physics.coefficient_of_friction() * force_gravity_normal_to_surface(mass_in_kg, slope_angle)
+        real_value = 0.0
+        for i in range(len(roots)):
+            if numpy.isreal(roots[i]):
+                real_value = numpy.real(roots[i])
 
-def force_gravity_normal_to_surface(mass_in_kg, slope_angle):
-    return mass_in_kg * Physics.gravitational_acceleration() * math.cos(math.radians(slope_angle))
+        return real_value
 
-def force_gravity_parallel_to_surface(mass_in_kg, slope_angle):
-    return mass_in_kg * Physics.gravitational_acceleration() * math.sin(math.radians(slope_angle))
+    def corner_radius_for_velocity_and_angle(self, velocity: float, cog_y: float, cog_z: float, slope_angle: float):
+        '''
+            Determines the minimum turn radius for a given velocity, ground angle
+            see: https://core.ac.uk/download/pdf/147124666.pdf
+        '''
+        coeff = math.pow(velocity, 2.0) / Physics.gravitational_acceleration()
 
-def velocity_for_slope_and_power(power, area_in_msq, mass_in_kg, slope_angle):
-    # F_aero * V + F_roll * V + F_hill * V - P = 0
-    coeff_cube = 0.5 * air_density * area_in_msq * Physics.coefficient_of_aerodynamic_drag()
-    coeff_square = 0.0
-    coeff_lin = force_rolling_friction(mass_in_kg, slope_angle) + force_gravity_parallel_to_surface(mass_in_kg, slope_angle)
+        angle = math.tan(math.radians(slope_angle))
 
-    roots = numpy.roots([coeff_cube, coeff_square, coeff_lin, -power])
+        ssf = cog_y / cog_z
 
-    real_value = 0.0
-    for i in range(len(roots)):
-     if numpy.isreal(roots[i]):
-         real_value = numpy.real(roots[i])
+        return coeff * ((ssf * angle + 1) / (ssf - angle))
 
-    return real_value
+    def wheel_force_and_power_with_cargo(
+        self,
+        velocity_in_meters_per_second,
+        slope_angle):
+        force_air = Physics.force_aerodynamic_friction(
+            self.frontal_surface(),
+            velocity_in_meters_per_second)
 
-def corner_radius_for_velocity_and_angle(velocity: float, cog_y: float, cog_z: float, slope_angle: float):
-    '''
-        Determines the minimum turn radius for a given velocity, ground angle
-        see: https://core.ac.uk/download/pdf/147124666.pdf
-    '''
-    coeff = math.pow(velocity, 2.0) / Physics.gravitational_acceleration()
+        force_roll = Physics.force_rolling_friction(
+            self.weight_with_cargo_in_kg(),
+            slope_angle)
 
-    angle = math.tan(math.radians(slope_angle))
+        force_slope = Physics.force_gravity_parallel_to_surface(
+            self.weight_with_cargo_in_kg(),
+            slope_angle
+        )
 
-    ssf = cog_y / cog_z
+        wheel_force = force_air + force_roll + force_slope
+        power = wheel_force * velocity_in_meters_per_second
 
-    return coeff * ((ssf * angle + 1) / (ssf - angle))
-
-def wheel_load(
-    area_in_msq,
-    velocity_in_meters_per_second,
-    mass_in_kg,
-    slope_angle):
-    force_air = force_aerodynamic_friction(
-        area_in_msq,
-        velocity_in_meters_per_second)
-
-    force_roll = force_rolling_friction(
-        mass_in_kg,
-        slope_angle)
-
-    force_slope = force_gravity_parallel_to_surface(
-        mass_in_kg,
-        slope_angle
-    )
-
-    wheel_force = force_air + force_roll + force_slope
-    power = wheel_force * velocity_in_meters_per_second
-
-    return wheel_force, power
+        return wheel_force, power
 
 
 
